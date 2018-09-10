a3055d462b4685f124c10ee642f90db861061588
anirudhvoruganti96 <anirudh96champ@gmail.com>
2018-09-10 09:26:18 +0000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,114 @@
+import RPi.GPIO as GPIO
+import time
+import cv2
+import numpy as np
+import time
+from playsound import playsound
+from threading import Thread
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from email.mime.image import MIMEImage
+import sys
+
+
+def play():
+    playsound('alert.mp3')
+
+
+def send_mail(attachment):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Alert"
+    msg['From'] = me
+    msg['To'] = you
+    fp = open(attachment, 'rb')
+    img = MIMEImage(fp.read())
+    fp.close()
+
+    msg.attach(img)
+
+    # Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+    s = smtplib.SMTP_SSL('smtp.gmail.com')
+    # uncomment if interested in the actual smtp conversation
+    #s.set_debuglevel(1)
+    # do the smtp auth; sends ehlo if it hasn't been sent already
+    s.login(me, my_password)
+
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
+
+def image_capture():
+        cap=cv2.VideoCapture(0)
+        ret,frame=cap.read()
+        #time.sleep(2000)
+        print(ret,frame)
+        cv2.imwrite('preview.jpg',frame)
+        time.sleep(10)
+        attachment='preview.jpg'
+        print('playing now!')
+
+        t1=Thread(target=play)
+        t1.start()
+
+        t2=Thread(target=send_mail,args=(attachment,))
+        t2.start()
+        #start_new_thread(send_mail,(attachment,))
+        print('Started thread 2')
+        t1.join(1)
+        t2.join(1)
+    
+        send_mail(attachment)
+
+        cap.release()
+        cv2.destroyAllWindows()
+        time.sleep(100)
+
+
+me = "yathisolutionstsiic@gmail.com"
+my_password = r"yathi12345"
+
+#change to whomever you wanted to send over heress
+you = "anvoruganti@gmail.com"
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(2, GPIO.IN) #PIR
+GPIO.setup(24, GPIO.OUT) #BUzzer
+
+while True:
+        a=int(GPIO.input(2))
+        print(a)
+        if a==0:
+            GPIO.output(24, True)
+            time.sleep(0.5) #Buzzer turns on for 0.5 sec
+            GPIO.output(24, False)
+            print("Motion Detected...")
+            time.sleep(5) #to avoid multiple detection
+            time.sleep(1)
+            cap=cv2.VideoCapture(0)
+            ret,frame=cap.read()
+            #time.sleep(2000)
+            print(ret,frame)
+            cv2.imwrite('preview.png',frame)
+            attachment='preview.png'
+            print('playing now!')
+            t1=Thread(target=play)
+            t1.start()
+
+            t2=Thread(target=send_mail,args=(attachment,))
+            t2.start()
+            #start_new_thread(send_mail,(attachment,))
+            print('Started thread 2')
+            t1.join(1)
+            t2.join(1)
+
+            cap.release()
+            cv2.destroyAllWindows()
+            
+            #image_capture()
+            
+        else:
+            print("Normal")
+            time.sleep(0.5) #Buzzer turns on for 0.5 sec
+           
+
